72b8e99fd52d7ee19f6ef1776adfc99610e0493f
Barry Hood <BarryPH@protonmail.com>
2017-06-02 15:22:07 +1200

    Replace styled-jsx with "styled-components"

---
+++ components/Home/Hero.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+import Block from '../shared/Block.js';
+import Container from '../shared/Container.js';
+
+const Wrapper = styled.div`
+	display: flex;
+`;
+
+const TextWrapper = styled.div`
+	margin-right: 2rem;
+`;
+
+const Title = styled.h1`
+	color: #D3145A;
+`;
+
+const QuoteForm = styled.form`
+	position: relative;
+	height: 3.5rem;
+	max-width: 500px;
+	margin-top: 2rem;
+`;
+
+const QuoteFormInput = styled.input`
+	width: 100%;
+	height: 100%;
+	padding: 0 1.5rem;
+	background-color: #fff;
+	border: 1px solid #ccc;
+`;
+
+// Use anchor component?
+const QuoteFormButton = styled.button`
+	--gutter: 0.4rem;
+	position: absolute;
+	right: var(--gutter);
+	top: var(--gutter);
+	padding: 0 1.5rem;
+	height: calc(100% - (var(--gutter) * 2));
+	background-color: #D3145A;
+	color: #fff;
+	border: 0;
+	font-weight: bold;
+`;
+
+const Hero = () => (
+	<Block altBackground>
+		<Container>
+			<Wrapper>
+				<TextWrapper>
+					<Title>We understand how valuable piece of mind is.</Title>
+					<p>Leave your apartment asset to VJ Ray and skip the hassle of management and maintenace.</p>
+
+					<QuoteForm>
+						<QuoteFormInput placeholder='Enter your email' />
+						<QuoteFormButton type='submit'>Get a quote today</QuoteFormButton>
+					</QuoteForm>
+				</TextWrapper>
+
+				<img src='http://lorempixel.com/400/300' />
+			</Wrapper>
+		</Container>
+	</Block>
+);
+
+export default Hero;
---
+++ css/global.js
@@ -0,0 +1,58 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+/* Type scale - generated from type-scale.com */
+
+html {font-size: 1em;}
+
+body {
+	background-color: white;
+	font-family: 'Open Sans', sans;
+	font-weight: 400;
+	line-height: 1.45;
+	color: #585870;
+}
+
+p {margin-bottom: 1.3em;}
+
+h1, h2, h3, h4 {
+	margin: 1.414em 0 0.5em;
+	font-weight: inherit;
+	line-height: 1.2;
+}
+
+h1 {
+	margin-top: 0.5em;
+	font-size: 3.157em;
+}
+
+h2 {font-size: 2.369em;}
+
+h3 {font-size: 1.777em;}
+
+h4 {font-size: 1.333em;}
+
+small, .font_small {font-size: 0.75em;}
+
+{ /* End of Type scale */ }
+
+
+*, *:before, *:after {
+	box-sizing: border-box;
+}
+
+body {
+	margin: 0;
+}
+
+a {
+	text-decoration: none;
+	color: inherit;
+}
+
+hr {
+	color: inherit;
+	border: 0;
+	border-bottom: 1px solid currentColor;
+}
+`;

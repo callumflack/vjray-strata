64e8cd44567594822dd876d3fcaa4802acf9efd1
Barry Hood <BarryPH@protonmail.com>
2017-06-15 14:18:14 +1200

    Setup storybook

---
+++ components/stories/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Button from '../shared/Button.js';
+
+storiesOf('Button', module)
+  .add('Default', () => (
+    <Button>Click me!</Button>
+  ))
+  .add('Icon', () => (
+    <Button icon>Click me!</Button>
+  ))
+  .add('Bordered', () => (
+    <Button bordered>Click me!</Button>
+  ))
+  .add('Clean with Icon', () => (
+    <Button clean icon>Click me!</Button>
+  ))
+  .add('Clean with theme color (brand)', () => (
+    <Button clean color='brand'>Click me!</Button>
+  ));

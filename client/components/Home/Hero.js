b9328998bfb265c0526ec1c56991df40938e5881
Barry Hood <BarryPH@protonmail.com>
2017-06-20 15:47:21 +1200

    Re add QuickQuoteForm in hero

--- client/components/Home/Hero.js
+++ client/components/Home/Hero.js
@@ -1,9 +1,7 @@
 import styled, { ThemeProvider } from 'styled-components';
-
 import { Flex, Box } from 'grid-styled';
-import { hoc } from '../styled-components'
-// import { space, width, fontSize, color } from 'styled-system'
 
+import { hoc } from '../styled-components'
 import theme from '../../css/theme.js';
 import Block from '../shared/Block.js';
 import Container from '../shared/Container.js';
@@ -27,42 +25,15 @@ const styledTheme = {
   ]
 }
 
-
-const BlockPosRelative = styled(Block)`
-  position: relative;
-`;
-
-// const Flex = styled.div`
-//   display: flex;
-// `;
-
 const TextWrapper = styled.div`
   margin-right: 2rem;
 `;
-
-// <BlockPosRelative altBackground>
-//   <Container>
-//     <Flex>
-//       <TextWrapper>
-//         <Headline>Skip the hassle of Strata management.</Headline>
-//         <Paragraph lg>At VJ Ray Strata, we understand how valuable piece of mind is.</Paragraph>
-//       </TextWrapper>
-//
-//       <img src='http://lorempixel.com/400/300' />
-//     </Flex>
-//   </Container>
-//
-//   <QuickQuoteForm />
-// </BlockPosRelative>
-
-// <Box width={theme.containers.lg} px={2} m='auto'>
-
 
 
 const Hero = () => (
   <div>
     <ThemeProvider theme={styledTheme}>
-      <Box width={theme.containers.lg} px={2} m='auto'>
+      <Box width={theme.containers.lg} px={2} m='auto' style={{position: 'relative'}}>
         <Flex
           style={{ height: '50vh' }}
           align='center'
@@ -72,10 +43,10 @@ const Hero = () => (
             <Paragraph fontSize={[ 3, 4 ]}>At VJ Ray Strata, we understand how valuable piece of mind is.</Paragraph>
           </Box>
         </Flex>
+
+        <QuickQuoteForm />
       </Box>
     </ThemeProvider>
-
-
   </div>
 );
 

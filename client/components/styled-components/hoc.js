49365682dd8c9c3a5ca3b0a845b75978a1d3de6d
Barry Hood <BarryPH@protonmail.com>
2017-06-22 16:05:18 +1200

    Use styled-system-dir instead of npm

--- client/components/styled-components/hoc.js
+++ client/components/styled-components/hoc.js
@@ -1,5 +1,5 @@
 import styled, {css} from 'styled-components';
-import { space, width, fontSize, color } from 'styled-system';
+import { space, width, fontSize, color } from './styled-system';
 // import { Flex, Box } from 'grid-styled';
 
 module.exports = Component => {

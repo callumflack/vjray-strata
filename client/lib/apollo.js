2a0ee5f688cab302c755d134162ed4693374ce03
Barry Hood <BarryPH@protonmail.com>
2017-06-22 14:30:14 +1200

    Export server uri to .env file

--- client/lib/apollo.js
+++ client/lib/apollo.js
@@ -4,6 +4,6 @@ import 'isomorphic-fetch';
 export default new ApolloClient({
   networkInterface: createNetworkInterface({
     ssrMode: !process.browser,
-    uri: 'http://localhost:4000/graphql',
+    uri: `${process.env.SERVER_URI}/graphql`,
   }),
 });
--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
+
+const webpack = require('webpack');
+
 module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' },
     }
-  }
+  },
+  webpack: (cfg) => {
+    cfg.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.SERVER_URI': JSON.stringify(process.env.SERVER_URI),
+      })
+    );
+
+    return cfg;
+  },
 };

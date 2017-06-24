dd1a89fb806b51f9a78d51a686a984271bfa907e
Callum Flack <callumflack@gmail.com>
2017-06-24 11:44:50 +1000

    add Head to Layout for reuse

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,17 +1,26 @@
 import { ThemeProvider } from 'styled-components'
+import Head from 'next/head'
+
 import globalStyles from './global.js'
 import theme from './theme.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
 
-const Layout = (props) => (
-  <ThemeProvider theme={theme}>
+// https://github.com/zeit/next.js/blob/master/examples/layout-component/components/layout.js
+
+export default ({ children, title = 'VJ Ray Strata, Sydney' }) => (
+  <ThemeProvider theme={ theme }>
     <div>
+      <Head>
+        <title>{ title }</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
 
       <Header />
 
       <main>
-        {props.children}
+        { children }
       </main>
 
       <Footer />
@@ -19,5 +28,3 @@ const Layout = (props) => (
     </div>
   </ThemeProvider>
 );
-
-export default Layout;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head'
 
 import styled from 'styled-components';
 import { Flex, Box } from '../components/styled-grid'
@@ -31,70 +30,62 @@ const StyledButton = styled(Button)`
 
 
 export default () => (
-  <div>
-    <Head>
-      <title>VJ Ray Strata, Sydney</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
+  <Layout>
+    <HeroBox />
 
-
-    <Layout>
-      <HeroBox />
-
-      <Block mx='lg'>
-        <Box width={[ 4/6, 5/6 ]} mx='auto'>
-          <Headline medium dark my={3} children='It all adds up to a better deal.' />
-          <LargeText grey>
-            "VJ Ray made everything easier. We all know our block is looked after, and friendly neighbours are far more easier to live with." — <AuthorText>Jessica, Campsie</AuthorText>
-          </LargeText>
-        </Box>
-        <Box width={[ 11/12, 1 ]} mx='auto' mt={4}>
-          <FeatureList />
-        </Box>
-      </Block>
-
-      <SydneyBox />
-
-      <Box px={3} py={[ 5, 6 ]}>
-        <Container textCenter>
-          <Display medium dark>Our duty is your satisfaction.</Display>
-          <Container width={[ 2/3, 1 ]}>
-            <LargeText active>"VJ Ray made everything easier. We all know our block is looked after, and friendly neighbours are far more easier to live with." — <AuthorText>Jessica, Campsie</AuthorText></LargeText>
-          </Container>
-        </Container>
-
-        <Container mx='lg' pt={[ 3, 4 ]}>
-          <Testimonials />
-        </Container>
+    <Block mx='lg'>
+      <Box width={[ 4/6, 5/6 ]} mx='auto'>
+        <Headline medium dark my={3} children='It all adds up to a better deal.' />
+        <LargeText grey>
+          "VJ Ray made everything easier. We all know our block is looked after, and friendly neighbours are far more easier to live with." — <AuthorText>Jessica, Campsie</AuthorText>
+        </LargeText>
       </Box>
+      <Box width={[ 11/12, 1 ]} mx='auto' mt={4}>
+        <FeatureList />
+      </Box>
+    </Block>
 
-      <Block mx='lg' bg='blue6'>
-        <Subheadline>Info on the go</Subheadline>
-        <Display active>Your guides to Sydney Strata.</Display>
-        <Container width={[ 1, 2/3 ]}>
-          <Text>Ipsum odit corrupti ullam in nam expedita corporis Magnam nemo corrupti suscipit recusandae voluptatem. Non aspernatur sed vero ipsam ut.</Text>
+    <SydneyBox />
+
+    <Box px={3} py={[ 5, 6 ]}>
+      <Container textCenter>
+        <Display medium dark>Our duty is your satisfaction.</Display>
+        <Container width={[ 2/3, 1 ]}>
+          <LargeText active>"VJ Ray made everything easier. We all know our block is looked after, and friendly neighbours are far more easier to live with." — <AuthorText>Jessica, Campsie</AuthorText></LargeText>
         </Container>
+      </Container>
 
-        <Guides />
-        <RecentPosts />
-      </Block>
+      <Container mx='lg' pt={[ 3, 4 ]}>
+        <Testimonials />
+      </Container>
+    </Box>
 
-      <Block>
-        <Subheadline dark>Get in touch</Subheadline>
-        <Display dark>Call 1300 073 405</Display>
-        <Container width={[ 1, 1/2 ]} mb={4}>
-          <HeadlineMeta white mb={3}>
-            <StyledButton fill icon bgColor='brand'>Get a fast quote</StyledButton>
-          </HeadlineMeta>
-          <Text fontSize={[ 3, 4 ]} grey>
-            Or drop-in without an appointment. We're open Monday to Saturday, 9am–5pm.
-          </Text>
-        </Container>
+    <Block mx='lg' bg='blue6'>
+      <Subheadline>Info on the go</Subheadline>
+      <Display active>Your guides to Sydney Strata.</Display>
+      <Container width={[ 1, 2/3 ]}>
+        <Text>Ipsum odit corrupti ullam in nam expedita corporis Magnam nemo corrupti suscipit recusandae voluptatem. Non aspernatur sed vero ipsam ut.</Text>
+      </Container>
 
-        <Contact />
-      </Block>
+      <Guides />
+      <RecentPosts />
+    </Block>
 
-    </Layout>
-  </div>
+    <Block>
+      <Subheadline dark>Get in touch</Subheadline>
+      <Display dark>Call 1300 073 405</Display>
+      <Container width={[ 1, 1/2 ]} mb={4}>
+        <HeadlineMeta white mb={3}>
+          <StyledButton fill icon bgColor='brand'>Get a fast quote</StyledButton>
+        </HeadlineMeta>
+        <Text fontSize={[ 3, 4 ]} grey>
+          Or drop-in without an appointment. We're open Monday to Saturday, 9am–5pm.
+        </Text>
+      </Container>
+
+      <Contact />
+    </Block>
+
+  </Layout>
+
 )

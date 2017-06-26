dc61d100b20efe46ea67f69437affb47e1279383
Barry Hood <BarryPH@protonmail.com>
2017-06-26 16:58:43 +1200

    Add font prop to hoc

--- client/components/styled-system/styled-components/hoc.js
+++ client/components/styled-system/styled-components/hoc.js
@@ -1,5 +1,6 @@
 const styled = require('styled-components').default
 const fontSize = require('../font-size')
+const fontFamily = require('../font-family')
 const width = require('../width')
 const space = require('../space')
 const color = require('../color')
@@ -7,6 +8,7 @@ const color = require('../color')
 module.exports = Component => {
   const SystemComponent = styled(Component)`
     ${fontSize}
+    ${fontFamily}
     ${width}
     ${space}
     ${color}

a10c4ca29bdb061f6e4a60ab2806810362efdc45
Callum Flack <callumflack@gmail.com>
2017-06-26 20:11:55 +1000

    add ternary for lineHeight string prop

--- client/components/styled-system/styled-components/hoc.js
+++ client/components/styled-system/styled-components/hoc.js
@@ -1,17 +1,25 @@
 const styled = require('styled-components').default
+const css = require('styled-components').css
 const fontSize = require('../font-size')
 const fontFamily = require('../font-family')
 const width = require('../width')
 const space = require('../space')
 const color = require('../color')
+const theme = require ('../../theme').default
 
-module.exports = Component => {
+// console.log( JSON.stringify(theme, null, 2) );
+
+module.exports = (Component, props) => {
   const SystemComponent = styled(Component)`
     ${fontSize}
     ${fontFamily}
     ${width}
     ${space}
     ${color}
+
+    ${props => props.lineHeight && css`
+        line-height: ${theme.lineHeight[props.lineHeight]};
+    `}
   `
 
   return SystemComponent

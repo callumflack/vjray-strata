9662e668d472565598342e3ea43efe13de850b1b
Callum Flack <callumflack@gmail.com>
2017-07-04 10:00:25 +1000

    add props for styling line-height, letter-spacing, display, text-align and text-transform

--- client/components/styled-system/styled-components/hoc.js
+++ client/components/styled-system/styled-components/hoc.js
@@ -17,8 +17,24 @@ module.exports = (Component, props) => {
     ${space}
     ${color}
 
+    ${props => props.inlineBlock && css`
+      display: inline-block;
+    `}
+
+    ${props => props.letterSpacing && css`
+      letter-spacing: ${theme.letterSpacing[props.letterSpacing] || theme.letterSpacing.body};
+    `}
+
     ${props => props.lineHeight && css`
-        line-height: ${theme.lineHeight[props.lineHeight]};
+      line-height: ${theme.lineHeight[props.lineHeight] || theme.lineHeight.text};
+    `}
+
+    ${props => props.align && css`
+      text-align: ${[props.align]};
+    `}
+
+    ${props => props.uppercase && css`
+      text-transform: uppercase;
     `}
   `
 

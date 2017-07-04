ee2f0d3c06dc9660421f26883bba32e9d08d089d
Barry Hood <BarryPH@protonmail.com>
2017-07-05 11:09:20 +1200

    Reformat form fields to one attirbute per line

--- client/components/shared/FullQuoteForm.js
+++ client/components/shared/FullQuoteForm.js
@@ -46,22 +46,52 @@ class FullQuoteForm extends React.Component {
         <FormGroup>
           <FormGroupHeadline>Your details</FormGroupHeadline>
 
-          <Input placeholder='Your name' name='name' validations='isExisty' validationError='Your name is required' />
-          <Input placeholder='Your email' name='email' validations={{isExisty: true, isEmail: true}} validationErrors={{isExisty: 'Your email is required', isEmail: 'Not a valid email'}} />
-          <Input placeholder='Your phone number' name='phoneNumber' validations='isExisty' validationError='Your phone number is required' />
+          <Input
+            placeholder='Your name'
+            name='name'
+            validations='isExisty'
+            validationError='Your name is required'
+          />
+          <Input
+            placeholder='Your email'
+            name='email'
+            validations={{isExisty: true, isEmail: true}}
+            validationErrors={{isExisty: 'Your email is required', isEmail: 'Not a valid email'}}
+          />
+          <Input
+            placeholder='Your phone number'
+            name='phoneNumber'
+            validations='isExisty'
+            validationError='Your phone number is required'
+          />
         </FormGroup>
 
         <FormGroup>
           <FormGroupHeadline>Strata details</FormGroupHeadline>
 
-          <Input placeholder='Your strata address' name='address' />
-          <Input placeholder='Your postcode' name='postcode' />
-          <Select placeholder='Total units in your strata scheme' name='unitsInScheme'>
+          <Input
+            placeholder='Your strata address'
+            name='address'
+          />
+          <Input
+            placeholder='Your postcode'
+            name='postcode'
+          />
+
+          <Select
+            placeholder='Total units in your strata scheme'
+            name='unitsInScheme'
+          >
             {Array(11).fill(0).map((item, i) =>
               <option value={i} key={i}>{i}</option>
             )}
           </Select>
-          <Textarea placeholder='Add any comments…' name='comments' rows='7' />
+
+          <Textarea
+            placeholder='Add any comments…'
+            name='comments'
+            rows='7'
+          />
         </FormGroup>
       </FormContainer>
     );

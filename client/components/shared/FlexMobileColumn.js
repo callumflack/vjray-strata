30f87c9bea5f04af63132cb4f95a8a4bb34dd318
Callum Flack <callumflack@gmail.com>
2017-07-05 15:08:50 +1000

    failing attempt at adding prop for cell spacing on mobile

--- client/components/shared/FlexMobileColumn.js
+++ client/components/shared/FlexMobileColumn.js
@@ -1,19 +1,25 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Flex } from '../styled-grid'
 import theme from '../theme'
 
 const FlexMobileColumn = styled(Flex)`
-    flex-direction: column;
+  flex-direction: column;
 
-    @media (min-width: ${theme.containers.sm}) {
-      flex-direction: row;
-    }
+  @media (min-width: ${theme.containers.sm}) {
+    flex-direction: row;
+  }
 
+  ${props => props.reverseShadow && css`
+    text-shadow: 0 0 15px rgba(0, 0, 0, 0.4);
+  `}
+
+  ${'' /* {$props => props.retainCellOrder && css`
     @media (max-width: ${theme.containers.sm}) {
       div + div {
         margin-top: ${theme.space[4]}px;
       }
     }
+  `} */}
 `
 
 export default FlexMobileColumn

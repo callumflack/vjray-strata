be1d0510d086c8694d8c2956e7f1bcb7805e5fcd
Barry Hood <BarryPH@protonmail.com>
2017-07-10 13:40:46 +1200

    Link contact maps to google maps

--- client/components/Shared/Contact.js
+++ client/components/Shared/Contact.js
@@ -7,6 +7,29 @@ import Button from '../styled-elements/Button'
 import { IconPhone, IconEmail, IconAddress, IconEmergency } from '../styled-elements/Icons'
 
 
+const locationList = [{
+  name: 'Campsie Head Office',
+  subOne: '8 Anglo Road',
+  subTwo: 'Campsie, NSW 2194',
+  phoneNumber: '1300 073 405',
+  email: 'campsie@vjray.com.au',
+  poBox: 'PO Box 369 Campsie, NSW 2194',
+  emergencyPhoneNumber: '1300 073 777',
+  googleMapsUrl: 'https://www.google.co.nz/maps/place/V+J+Ray/@-33.9113278,151.1011612,17z',
+  imageName: 'map.jpg',
+}, {
+  name: 'Caringbah Office',
+  subOne: '18 President Avenue',
+  subTwo: 'Caringbah, NSW 2229',
+  phoneNumber: '1300 073 405',
+  email: 'campsie@vjray.com.au',
+  poBox: 'PO Box 369 Campsie, NSW 2194',
+  emergencyPhoneNumber: '1300 073 777',
+  googleMapsUrl: 'https://www.google.co.nz/maps/place/V.J.+Ray/@-34.0432583,151.1198929,17z',
+  imageName: 'map.jpg',
+}];
+
+
 const Locations = styled(Flex)`
   flex-direction: column;
   text-align: left;
@@ -16,11 +39,56 @@ const Locations = styled(Flex)`
   }
 `;
 
-const Location = styled(Box)`
+const LocationRoot = styled(Box)`
   @media (max-width: 699px) {
     margin-top: ${theme.space[4]}px;
   }
 `;
+
+const Location = (props) => (
+  <LocationRoot
+    width={[ 1, 1/3 ]}
+    px={[ 3, 2 ]}>
+    <Link href={props.location.googleMapsUrl}>
+      <a>
+        <Image src={'/static/img/' + props.location.imageName} />
+      </a>
+    </Link>
+
+    <Box my={2}>
+      <Link href={props.location.googleMapsUrl}>
+        <a>
+          <Text color='brandAlt' font='textMedium' mb='6px'>{props.location.name}</Text>
+        </a>
+      </Link>
+
+      <Text color='text70'>{props.location.subOne}</Text>
+      <Text color='text70'>{props.location.subTwo}</Text>
+    </Box>
+
+    <Box>
+      <Link href='tel:{props.location.phoneNumber}'>
+        <a><Item borderTop>
+          <IconPhone contact />{props.location.phoneNumber}
+        </Item></a>
+      </Link>
+      <Link href='mailto:{props.location.email}'>
+        <a><Item>
+          <IconEmail contact />{props.location.email}
+        </Item></a>
+      </Link>
+      <Item>
+        <IconAddress contact />{props.location.poBox}
+      </Item>
+      <Link href='tel:{props.location.emergencyPhoneNumber}'>
+        <a><Item>
+          <IconEmergency contact />Emergency {props.location.emergencyPhoneNumber}
+        </Item></a>
+      </Link>
+
+    </Box>
+  </LocationRoot>
+);
 
 const Image = styled.img`
   ${'' /* border: 1px solid ${theme.colors.text40}; */}
@@ -42,43 +110,9 @@ const Contact = (props) => (
     justify='center'
     mx={-2}>
 
-    {Array(2).fill(1).map(() =>
-
-    <Location
-      width={[ 1, 1/3 ]}
-      px={[ 3, 2 ]}>
-      <Image src='/static/img/map.jpg' />
-
-      <Box my={2}>
-        <Text color='brandAlt' font='textMedium' mb='6px'>Campsie Head Office</Text>
-        <Text color='text70'>8 Anglo Road</Text>
-        <Text color='text70'>Campsie, NSW 2194</Text>
-      </Box>
-
-      <Box>
-        <Link href='tel:1300 073 405'>
-          <a><Item borderTop>
-            <IconPhone contact />1300 073 405
-          </Item></a>
-        </Link>
-        <Link href='mailto:campsie@vjray.com.au'>
-          <a><Item>
-            <IconEmail contact />campsie@vjray.com.au
-          </Item></a>
-        </Link>
-        <Item>
-          <IconAddress contact />PO Box 369 Campsie, NSW 2194
-        </Item>
-        <Link href='tel:1300 073 777'>
-          <a><Item>
-            <IconEmergency contact />Emergency 1300 073 777
-          </Item></a>
-        </Link>
-
-      </Box>
-    </Location>
+    {locationList.map((location, i) =>
+      <Location location={location} key={i} />
     )}
-
   </Locations>
 );
 

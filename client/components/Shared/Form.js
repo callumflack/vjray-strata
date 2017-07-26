b039aed58cc609bbdbd03e45bc6e1633713c4d37
Callum Flack <callumflack@gmail.com>
2017-07-26 14:41:54 +1000

    put styles first

--- client/components/Shared/Form.js
+++ client/components/Shared/Form.js
@@ -6,6 +6,79 @@ import theme from '../theme'
 import { Display, BoxedSubheadline, HeadlineDetail } from '../styled-elements/Headline'
 
 
+const Form = styled(Formsy.Form)`
+  --elementHeight: 54px;
+
+  input,
+  select,
+  textarea,
+  button {
+    margin-top: 1rem;
+  }
+
+  input,
+  select,
+  textarea {
+    border: 1px solid ${theme.colors.text20};
+    height: var(--elementHeight);
+    padding: 0 1rem;
+    transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
+    resize: none;
+    width: 100%;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+
+    &:focus,
+    &:active,
+    &:hover,
+    &:visited {
+      outline: 0;
+    }
+
+    &:focus {
+      border-color: ${theme.colors.brand};
+      box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(102, 175, 233, 0.6);
+    }
+
+    &::-webkit-input-placeholder { color: ${theme.colors.text40}; }
+    &::-moz-placeholder          { color: ${theme.colors.text40}; }
+    &:-ms-input-placeholder      { color: ${theme.colors.text40}; }
+    &:-moz-placeholder           { color: ${theme.colors.text40}; }
+  }
+
+  textarea {
+    min-height: var(--elementHeight);
+    height: auto;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+`;
+
+const FormHeader = props =>
+  <BoxedSubheadline bg='brand' color='white' {...props} />
+
+const FormGroup = styled.div`
+  & + & {
+    margin-top: 3rem;
+  }
+`;
+
+const FormGroupHeadline = styled(HeadlineDetail)`
+  margin-left: 1rem;
+`;
+
+const SuccessMessage = styled.div`
+  color: #42db41;
+  text-align: center;
+  margin-bottom: 0.5rem;
+`;
+
+const ErrorMessage = styled.div`
+  color: #f23030;
+  margin-top: 0.5rem;
+`;
+
+
 class InputRoot extends React.Component {
   render() {
     return (
@@ -55,63 +128,6 @@ class TextareaRoot extends React.Component {
 const Input = HOC(InputRoot);
 const Select = HOC(SelectRoot);
 const Textarea = HOC(TextareaRoot);
-
-
-const Form = styled(Formsy.Form)`
-  --elementHeight: 54px;
-
-  input,
-  select,
-  textarea,
-  button {
-    margin-top: 1rem;
-  }
-
-  input,
-  select,
-  textarea {
-    border: 1px solid ${theme.colors.text20};
-    height: var(--elementHeight);
-    padding: 0 1rem;
-    width: 100%;
-
-    &::-webkit-input-placeholder { color: ${theme.colors.text40}; }
-    &::-moz-placeholder          { color: ${theme.colors.text40}; }
-    &:-ms-input-placeholder      { color: ${theme.colors.text40}; }
-    &:-moz-placeholder           { color: ${theme.colors.text40}; }
-  }
-
-  textarea {
-    min-height: var(--elementHeight);
-    height: auto;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-  }
-`;
-
-const FormHeader = props =>
-  <BoxedSubheadline bg='brand' color='white' {...props} />
-
-const FormGroup = styled.div`
-  & + & {
-    margin-top: 3rem;
-  }
-`;
-
-const FormGroupHeadline = styled(HeadlineDetail)`
-  margin-left: 1rem;
-`;
-
-const SuccessMessage = styled.div`
-  color: #42db41;
-  text-align: center;
-  margin-bottom: 0.5rem;
-`;
-
-const ErrorMessage = styled.div`
-  color: #f23030;
-  margin-top: 0.5rem;
-`;
 
 export {
   Form,

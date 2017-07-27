d4a09a4724d9a81378dacf1fa26c9d93e970e36f
Callum Flack <callumflack@gmail.com>
2017-07-27 18:07:35 +1000

    add emergencies pg link placeholder

--- client/components/Shared/Footer.js
+++ client/components/Shared/Footer.js
@@ -90,6 +90,7 @@ const Footer = (props) => (
               <Link href='/contact'><a><span>Contact us</span></a></Link>
               <Link href='/fast-quote'><a>Get a fast quote</a></Link>
               <Link href='/contact'><a>Report an issue</a></Link>
+              <Link href='/contact'><a>Emergencies</a></Link>
             </Column>
           </Nav>
 

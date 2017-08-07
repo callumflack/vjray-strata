39ccb0ebaa6e158774ecb9675cb82e06de2e6be6
Callum Flack <callumflack@gmail.com>
2017-08-07 17:54:53 +1000

    WIP layout styling

--- client/pages/useful-info/forms-and-fact-sheets.js
+++ client/pages/useful-info/forms-and-fact-sheets.js
@@ -6,53 +6,72 @@ import apollo from '../../lib/apollo'
 
 import { Box, Flex } from '../../components/styled-grid'
 
+import theme from '../../components/theme'
+
+import { hoc } from '../../components/styled-elements/hoc'
+// import { hoc } from '../../components/styled-system/styled-components'
 import Layout from '../../components/styled-elements/Layout'
 import Container from '../../components/styled-elements/Container'
 import { Display, Subheadline } from '../../components/styled-elements/Headline'
-import { LargeText, MediumText, LineBreak } from '../../components/styled-elements/Text'
+import { LargeText, MediumText, InlineText, LineBreak } from '../../components/styled-elements/Text'
 
-import HeroBox from '../../components/Shared/HeroBox'
+import ColorBox from '../../components/Shared/ColorBox'
 import Footer from '../../components/Shared/Footer'
 import Header from '../../components/Shared/Header'
 
 
-const WhiteHeroBox = HeroBox.extend`
+const StyledColorBox = styled(ColorBox)`
+  background-color: transparent;
+  background-image:
+    linear-gradient(
+      to bottom,
+      rgba(74, 74, 85, 0.2),
+      rgba(74, 74, 85, 0) 50%
+    );
+  background-image: none;
   background-color: white;
-  height: auto;
-`;
+`
 
 const ListItemRoot = styled(Box)`
-  border-bottom: 1px solid #f2f2f2;
+  border-bottom: 1px solid ${theme.colors.text20};
 
   &:first-child {
     padding-top: 0;
   }
 `;
 
+// const ItemLedeRoot = hoc('span').extend``
+// const ItemLedeRoot = styled('span')``
+// const ItemRoot = hoc('span').extend``
+
+const ItemLede = props =>
+  <InlineText
+    color='text70'
+    font='textLight'
+    fontSize={[ 3, 4 ]}
+    {...props}
+  />
+
+
 const ListItem = props => (
   <ListItemRoot py={3}>
     <Flex direction={[ 'column', 'row' ]} align={[ 'flex-start', 'flex-end' ]}>
       <Link href={`${props.fileUrl}`}>
         <a>
-          <ItemHeadline>{props.headline}</ItemHeadline>
-          <MediumText pt={[ 1, 0 ]} color='text70'>{props.subheadline}</MediumText>
+          <LargeText color='text'>{props.headline}</LargeText>
+          <ItemLede>{props.subheadline}</ItemLede>
         </a>
       </Link>
     </Flex>
   </ListItemRoot>
 );
 
-const ItemHeadline = styled.h1`
-  font-weight: bold;
-  margin-right: 2rem;
-`;
-
 
 const Root = (props) => (
   <Layout>
     <Header pathname={props.pathname} clear />
 
-    <WhiteHeroBox pb={[ 4, 5, 6 ]} px={3}>
+    <StyledColorBox pb={[ 4, 5, 6 ]} px={3}>
       <Container mw='rg' mt={[ 3, 4, 5 ]}>
         <Box width={[ 1, 1, 2/3, 2/3 ]} mt={[ 0, 3 ]}>
           <Subheadline color='brand' children='Info on the go' />
@@ -60,43 +79,45 @@ const Root = (props) => (
             Strata forms <LineBreak>and fact sheets.</LineBreak>
           </Display>
           <LargeText color='text70'>
-            Everything you might need to <LineBreak>know about your Strata management. </LineBreak>
+            Readily available PDF downloads <LineBreak>to help your Strata management.</LineBreak>
           </LargeText>
         </Box>
       </Container>
-    </WhiteHeroBox>
+    </StyledColorBox>
 
-    <Box p={3}>
-      <Container>
-        <Subheadline children='Forms' />
+    <Box py={[ 4, 5, 6 ]} px={3}>
+      <Box p={3}>
+        <Container>
+          <Subheadline children='Forms' />
 
-        <div>
-          {props.forms.map(form => (
-            <ListItem
-              headline={form.headline}
-              subheadline={form.subheadline}
-              fileUrl={form.file.url}
-              key={form._id}
-            />
-          ))}
-        </div>
-      </Container>
-    </Box>
+          <div>
+            {props.forms.map(form => (
+              <ListItem
+                headline={form.headline}
+                subheadline={form.subheadline}
+                fileUrl={form.file.url}
+                key={form._id}
+              />
+            ))}
+          </div>
+        </Container>
+      </Box>
 
-    <Box p={3}>
-      <Container>
-        <Subheadline children='Fact sheets' />
+      <Box p={3}>
+        <Container>
+          <Subheadline children='Fact sheets' />
 
-        <div>
-          {props.factSheets.map(factSheet => (
-            <ListItem
-              headline={factSheet.headline}
-              subheadline={factSheet.subheadline}
-              key={factSheet._id}
-            />
-          ))}
-        </div>
-      </Container>
+          <div>
+            {props.factSheets.map(factSheet => (
+              <ListItem
+                headline={factSheet.headline}
+                subheadline={factSheet.subheadline}
+                key={factSheet._id}
+              />
+            ))}
+          </div>
+        </Container>
+      </Box>
     </Box>
 
     <Footer />

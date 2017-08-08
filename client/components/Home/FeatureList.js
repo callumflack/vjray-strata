8ae25956a2572fa67b12496ece72c124f923989e
Callum Flack <callumflack@gmail.com>
2017-08-08 13:31:22 +1000

    add signed-off feature icons

--- client/components/Home/FeatureList.js
+++ client/components/Home/FeatureList.js
@@ -2,15 +2,23 @@ import styled from 'styled-components'
 import { Flex, Box } from '../styled-grid'
 import hoc from '../styled-elements/hoc'
 import theme from '../theme.js'
-// import { tint } from 'polished'
 import { Text, LargeText } from '../styled-elements/Text.js'
 import { Headline } from '../styled-elements/Headline.js'
-import { IconSun } from '../styled-elements/Icons.js';
+import {
+  IconSun,
+  IconEmergencyContact,
+  IconService,
+  IconLockin,
+  IconExperience
+} from '../styled-elements/Icons.js';
+import Icon from '../styled-elements/Icon.js';
+import { icons } from '../constants'
 
 
 const Header = (props) =>
-  <Headline font='displayMedium' fontSize={[ 3, 4 ]} my={2} />
+  <Headline font='displayMedium' fontSize={[ 3, 4 ]} my={2} {...props} />
 
+// import { tint } from 'polished'
 // color: ${tint(0.7, ${theme.colors.text} )}
 // color: ${tint(0.7, '#585870')};
 const Lede = styled(Text)`
@@ -26,32 +34,33 @@ const Divider = () => (
 const FeatureList = () => (
   <Flex>
     <Box>
-      <IconSun />
-      <Header>Dedicated & experienced</Header>
+      <IconExperience color='brand' />
+      {/* <Icon icon color='brand'={ icons.Experience } height='100' width='100' /> */}
+      <Header>Over half a century young</Header>
       <Lede>Tell us about your home and what makes it unique lorem ipsum.</Lede>
     </Box>
 
     <Divider />
 
     <Box>
-      <IconSun />
-      <Header>Navigate the system</Header>
+      <IconLockin color='brand' />
+      <Header>No lock-in contracts</Header>
       <Lede>Tell us about your home and what makes it unique lorem ipsum.</Lede>
     </Box>
 
     <Divider />
 
     <Box>
-      <IconSun />
-      <Header>Attendance in person</Header>
+      <IconService color='brand' />
+      <Header>Open 6 days a week</Header>
       <Lede>Tell us about your home and what makes it unique lorem ipsum.</Lede>
     </Box>
 
     <Divider />
 
     <Box>
-      <IconSun />
-      <Header>No lock-ins Full access.</Header>
+      <IconEmergencyContact color='brand' />
+      <Header>24/7 emergency contact</Header>
       <Lede>Tell us about your home and what makes it unique lorem ipsum.</Lede>
     </Box>
   </Flex>

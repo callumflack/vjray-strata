3977714ac0d80d40452821ba043b30f475ef127d
Barry Hood <BarryPH@protonmail.com>
2017-09-05 16:03:40 +1200

    Add react-ga tracking

--- client/components/Shared/Layout.js
+++ client/components/Shared/Layout.js
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import theme from '../theme';
 import globalStyles from './global'
+import GaWrapper from '../../lib/ga-wrapper.js'
+
 
 // https://github.com/zeit/next.js/blob/master/examples/layout-component/components/layout.js
 
-export default ({ children, title = 'VJ Ray Strata Management, Sydney' }) => (
+const Layout = ({ children, title = 'VJ Ray Strata Management, Sydney' }) => (
   <ThemeProvider theme={ theme }>
     <div>
       <Head>
@@ -19,3 +21,5 @@ export default ({ children, title = 'VJ Ray Strata Management, Sydney' }) => (
     </div>
   </ThemeProvider>
 );
+
+export default GaWrapper(Layout);

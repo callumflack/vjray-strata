b2fa2cebf27bae1737e5f94465ad23756a7e4993
Barry Hood <BarryPH@protonmail.com>
2017-10-19 09:33:13 +1300

    Wrap FormContainer children in div
    FormContainer should only have one child

--- client/components/Shared/FormQuote.js
+++ client/components/Shared/FormQuote.js
@@ -40,71 +40,73 @@ class FormQuote extends React.Component {
   render() {
     return (
       <FormContainer brand submit={this.submit}>
-        <FormGroup>
-          <FormGroupHeadline>Your details (Required)</FormGroupHeadline>
+        <div>
+          <FormGroup>
+            <FormGroupHeadline>Your details (Required)</FormGroupHeadline>
 
-          <Input
-            placeholder='Your name'
-            name='name'
-            required
-          />
-          <Input
-            placeholder='Your email'
-            name='email'
-            validations={{
-              isEmail: true
-            }}
-            validationErrors={{
-              isEmail: 'Not a valid email'
-            }}
-            required
-          />
-          <Input
-            placeholder='Your phone number'
-            name='phoneNumber'
-            validations='isExisty'
-            required
-          />
-        </FormGroup>
+            <Input
+              placeholder='Your name'
+              name='name'
+              required
+            />
+            <Input
+              placeholder='Your email'
+              name='email'
+              validations={{
+                isEmail: true
+              }}
+              validationErrors={{
+                isEmail: 'Not a valid email'
+              }}
+              required
+            />
+            <Input
+              placeholder='Your phone number'
+              name='phoneNumber'
+              validations='isExisty'
+              required
+            />
+          </FormGroup>
 
-        <FormGroup>
-          <FormGroupHeadline>Strata details (required)</FormGroupHeadline>
+          <FormGroup>
+            <FormGroupHeadline>Strata details (required)</FormGroupHeadline>
 
-          <Input
-            placeholder='Your strata address'
-            name='address'
-            required
-          />
-          <Input
-            placeholder='Your postcode'
-            name='postcode'
-            required
-          />
+            <Input
+              placeholder='Your strata address'
+              name='address'
+              required
+            />
+            <Input
+              placeholder='Your postcode'
+              name='postcode'
+              required
+            />
 
-          <Select
-            name='unitsInScheme'
-            defaultValue=''
-            required
-          >
-            <option value='' selected hidden>Total units in your strata</option>
-            <option value="1-10">up to 10</option>
-            <option value="10-20">10-20</option>
-            <option value="20-40">20-40</option>
-            <option value="40+">40+</option>
-          </Select>
+            <Select
+              name='unitsInScheme'
+              defaultValue=''
+              required
+            >
+              <option value='' selected hidden>Total units in your strata</option>
+              <option value="1-10">up to 10</option>
+              <option value="10-20">10-20</option>
+              <option value="20-40">20-40</option>
+              <option value="40+">40+</option>
+            </Select>
 
-          <Textarea
-            placeholder='What&#39;s the problem we can help you with?'
-            name='comments'
-            rows='7'
-            required
-          />
+            <Textarea
+              placeholder='What&#39;s the problem we can help you with?'
+              name='comments'
+              rows='7'
+              required
+            />
 
-        </FormGroup>
+          </FormGroup>
 
-        {/* Formspree filters */}
-        <input type="text" name="_gotcha" style={{display: "none" }} />
-        <input type="hidden" name="_subject" value="Strata website new business submission" />
+          {/* Formspree filters */}
+          <input type="text" name="_gotcha" style={{display: "none" }} />
+          <input type="hidden" name="_subject" value="Strata website new business submission" />
+        </div>
       </FormContainer>
     );
   }

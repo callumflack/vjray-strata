b0c89f12f540d36c5ba18f63b60d65c9ed7bca88
Callum Flack <callumflack@gmail.com>
2017-10-18 14:11:52 +1000

    add empty dataLayer ready for adword events setup

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -7,9 +7,11 @@ export default class MyDocument extends Document {
     const main = sheet.collectStyles(<Main />);
     const styleTags = sheet.getStyleTags();
 
-    return (
-      <html>
+    return <html>
         <Head>
+          <script>
+            dataLayer = [];
+          </script>
           <script src="/static/js/google-tag-manager.js" />
           <script src="/static/js/google-phone-call-tracking.js" />
 
@@ -17,12 +19,11 @@ export default class MyDocument extends Document {
           <style dangerouslySetInnerHTML={{ __html: styleTags }} />
         </Head>
         <body>
-          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNFDHLZ" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNFDHLZ" height="0" width="0" style="display:none;visibility:hidden;"></iframe>` }} />
 
           <div className="root">{main}</div>
           <NextScript />
         </body>
-      </html>
-    );
+      </html>;
   }
 }

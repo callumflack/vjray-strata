8bde7cb3fc8049252de81052262d5d19437c5d9a
Callum Flack <callumflack@gmail.com>
2017-11-14 14:05:33 +1000

    initial go at merging in the Shared components w/ their required restylings

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,28 +1,32 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  render () {
-    const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleTags()
+  render() {
+    const sheet = new ServerStyleSheet();
+    const main = sheet.collectStyles(<Main />);
+    const styleTags = sheet.getStyleTags();
 
     return (
-      <html>
+      <html lang="en">
         <Head>
-          <script src='/static/js/google-tag-manager.js'></script>
-          <script src='/static/js/google-phone-call-tracking.js'></script>
-
+          <script>dataLayer = [];</script>
+          <script src="/static/js/google-tag-manager.js" />
           <title>My page</title>
           <style dangerouslySetInnerHTML={{ __html: styleTags }} />
         </Head>
         <body>
-          <div className='root'>
-            {main}
-          </div>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html:
+                "<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-TNFDHLZ\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden;\"></iframe>",
+            }}
+          />
+          <div className="root">{main}</div>
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
--- client/pages/contact.js
+++ client/pages/contact.js
@@ -20,24 +20,27 @@ import {
   HeadlineMeta
 } from "../components/Shared/Headline";
 import {
-  Text,
-  SmallText,
   LargeText,
-  LineBreak
-} from "../components/Shared/Text";
-import Button from "../components/Shared/Button";
+  SmallText,
+  PrimaryButtonText
+} from "../components/Shared/Texts";
+import LineBreak from "../components/Shared/LineBreak";
+import { Button } from "../components/Shared/Buttons";
 
 import Header from "../components/Shared/Header";
 import Footer from "../components/Shared/Footer";
 import Block from "../components/Shared/Block";
 import HeroBlock from "../components/Shared/HeroBlock";
 import HGroup from "../components/Shared/HGroup";
+
+import Contacts from "../components/Shared/Contacts";
+import ContactForms from "../components/Shared/ContactForms";
+
 import FormContact from "../components/Shared/FormContact";
 import FormIssue from "../components/Shared/FormIssue";
 import { FormHeader } from "../components/Shared/Form.js";
 import { Tabs, Pane } from "../components/Shared/Tabs.js";
 import ContactActionAlt from "../components/ContactActionAlt";
-import Contacts from "../components/Shared/Contacts";
 import LargeButtonStyler from "../components/LargeButtonStyler";
 
 const Root = props => (
@@ -57,52 +60,58 @@ const Root = props => (
       />
     </HeroBlock>
 
-    <Block textCenter pb={[0, 0]}>
-      <Display color="brandAlt" mb={1} children="Visit us anytime." />
-      <LargeText color="text70" children="No appointment necessary." />
-      <Box mt={0}>
-        <Contacts pathname={props.pathname} />
-        <SmallText color="text70" mt={[3, 4]}>
-          Looking for the Auburn office? They aren't affiliated with us.{" "}
-          <LineBreak mx="auto">
-            You can find them{" "}
-            <Link href="http://vjrayauburn.com.au/">here.</Link>
-          </LineBreak>
-        </SmallText>
-      </Box>
+    <Block
+      maxWidth="38em"
+      title="Visit us anytime."
+      titleColor="brand"
+      text="No appointment needed."
+      noBottomPadding
+    >
+      <Contacts pathname={props.pathname} />
+      <SmallText center color="text70" mt={[3, 4]}>
+        Looking for the Auburn office? They aren&apos;t affiliated with us.
+        <LineBreak mx="auto">
+          You can find them <Link href="http://vjrayauburn.com.au/">here.</Link>
+        </LineBreak>
+      </SmallText>
     </Block>
 
-    <Block id="contact-forms" mw="sm" pb={[0, 0]}>
-      <Container textCenter>
-        <Display color="brandAlt" mb={1} children="Write to us." />
-        <LargeText color="text70">We'll respond quick smart.</LargeText>
-      </Container>
-
-      <Box mt={[3, 3, 3, 4]}>
-        <Tabs selected={props.defaultForm === "report" ? 1 : 0}>
-          {props.tabs.map(tab => (
-            <Pane label={tab.name} key={tab._id}>
-              {tab.content}
-            </Pane>
-          ))}
-        </Tabs>
-      </Box>
+    <Block
+      id="contact-forms"
+      maxWidth="38em"
+      title="Write to us."
+      titleColor="brand"
+      text="We'll respond quick smart."
+      noBottomPadding
+    >
+      <ContactForms defaultForm={props.defaultForm} />
     </Block>
 
-    <Block textCenter>
-      <Display color="brand" mb={1} children="Is it an emergency?" />
-      <LargeText color="text70" mb={3}>
-        For all types of after-hours repairs.
-      </LargeText>
-      <LargeButtonStyler>
-        <Link href="/emergency">
-          <Button large icon bgColor="brand" children="Emergencies" />
-        </Link>
-      </LargeButtonStyler>
+    <Block
+      id="emergencies"
+      title="Is it a Strata emergency?"
+      titleColor="brand"
+      text="Go to our after-hours Strata repairs."
+    >
+      <a href="https://vjraystrata.com.au/emergency">
+        <PrimaryButtonText center>
+          <Button icon bg="brand" children="Emergencies" />
+        </PrimaryButtonText>
+      </a>
     </Block>
 
-    <Block border>
-      <ContactActionAlt btnColor="brandAlt" />
+    <Block
+      border
+      maxWidth="38em"
+      subhead="Learn more"
+      title="Skip the hassle of Strata Management"
+      titleColor="text"
+    >
+      <a href="let-us-help-you">
+        <PrimaryButtonText center>
+          <Button icon bg="brand" children="Get a fast quote" />
+        </PrimaryButtonText>
+      </a>
     </Block>
 
     <Footer />

11ac66019338c7e1b7b0fd22ccfb05fbbe583ab3
Callum Flack <callumflack@gmail.com>
2017-11-14 13:29:02 +1000

    intiail update

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -1,112 +1,138 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-import gql from 'graphql-tag'
-import apollo from '../lib/apollo'
+import gql from "graphql-tag";
+import apollo from "../lib/apollo";
 
-import { contactDetails } from '../components/constants'
+import { contactDetails } from "../components/constants";
+import theme from "../components/theme";
+import styled, { css } from "styled-components";
+import { Flex, Box } from "rebass";
 
-import theme from '../components/theme'
-import styled, { css } from 'styled-components'
-import { Flex, Box } from 'rebass'
+import Layout from "../components/Shared/Layout";
+import Container from "../components/Shared/Container";
+import CoverImage from "../components/Shared/CoverImage";
+import {
+  Display,
+  Headline,
+  Subheadline,
+  MobileSubheadline,
+  HeadlineMeta
+} from "../components/Shared/Headline";
+import {
+  Text,
+  SmallText,
+  LargeText,
+  LineBreak
+} from "../components/Shared/Text";
+import Button from "../components/Shared/Button";
 
-import Layout from '../components/Shared/Layout'
-import Container from '../components/Shared/Container'
-import CoverImage from '../components/Shared/CoverImage'
-import { Display, Headline, Subheadline, MobileSubheadline, HeadlineMeta } from '../components/Shared/Headline'
-import { Text, SmallText, LargeText, LineBreak } from '../components/Shared/Text'
-import Button from '../components/Shared/Button'
-
-import Header from '../components/Shared/Header'
-import Footer from '../components/Shared/Footer'
-import Block from '../components/Shared/Block'
-import HeroContact from '../components/HeroContact'
-import FormContact from '../components/Shared/FormContact'
-import FormIssue from '../components/Shared/FormIssue'
-import { FormHeader } from '../components/Shared/Form.js'
-import { Tabs, Pane } from '../components/Shared/Tabs.js'
-import ContactActionAlt from '../components/ContactActionAlt'
-import Contacts from '../components/Shared/Contacts'
-import LargeButtonStyler from '../components/LargeButtonStyler'
-
-
+import Header from "../components/Shared/Header";
+import Footer from "../components/Shared/Footer";
+import Block from "../components/Shared/Block";
+import HeroBlock from "../components/Shared/HeroBlock";
+import HGroup from "../components/Shared/HGroup";
+import FormContact from "../components/Shared/FormContact";
+import FormIssue from "../components/Shared/FormIssue";
+import { FormHeader } from "../components/Shared/Form.js";
+import { Tabs, Pane } from "../components/Shared/Tabs.js";
+import ContactActionAlt from "../components/ContactActionAlt";
+import Contacts from "../components/Shared/Contacts";
+import LargeButtonStyler from "../components/LargeButtonStyler";
 
 const Root = props => (
   <Layout>
-    <Header
-      pathname={props.pathname}
-      clear
-      color='brandAlt'
-    />
-    <HeroContact />
+    <Header pathname={props.pathname} clear color="brandAlt" />
 
-    <Block textCenter pb={[ 0, 0 ]}>
-      <Display color='brandAlt' mb={1} children='Visit us anytime.' />
-      <LargeText color='text70' children='No appointment necessary.' />
+    <HeroBlock
+      bluegreyGradientBg
+      img="http://res.cloudinary.com/pw-img-cdn/image/upload/v1503618861/hero-contact-_awgf0r.png"
+    >
+      <HGroup
+        title="How can we help make your Strata living easier?"
+        titleColor="text"
+        titleFamily="displayMedium"
+        text="Visit, call or write to us 24/7."
+        textColor="text70"
+      />
+    </HeroBlock>
+
+    <Block textCenter pb={[0, 0]}>
+      <Display color="brandAlt" mb={1} children="Visit us anytime." />
+      <LargeText color="text70" children="No appointment necessary." />
       <Box mt={0}>
         <Contacts pathname={props.pathname} />
-        <SmallText color='text70' mt={[ 3, 4 ]}>
-          Looking for the Auburn office? They aren't affiliated with us. <LineBreak mx='auto'>You can find them <Link href='http://vjrayauburn.com.au/'>here.</Link></LineBreak>
+        <SmallText color="text70" mt={[3, 4]}>
+          Looking for the Auburn office? They aren't affiliated with us.{" "}
+          <LineBreak mx="auto">
+            You can find them{" "}
+            <Link href="http://vjrayauburn.com.au/">here.</Link>
+          </LineBreak>
         </SmallText>
       </Box>
     </Block>
 
-    <Block id='contact-forms' mw='sm' pb={[ 0, 0 ]}>
+    <Block id="contact-forms" mw="sm" pb={[0, 0]}>
       <Container textCenter>
-        <Display color='brandAlt' mb={1} children='Write to us.' />
-        <LargeText color='text70'>We'll respond quick smart.</LargeText>
+        <Display color="brandAlt" mb={1} children="Write to us." />
+        <LargeText color="text70">We'll respond quick smart.</LargeText>
       </Container>
 
-      <Box mt={[ 3, 3, 3, 4]}>
-        <Tabs selected={props.defaultForm === 'report' ? 1 : 0}>
-          {props.tabs.map(tab =>
-            <Pane label={tab.name} key={tab._id}>{tab.content}</Pane>)
-          }
+      <Box mt={[3, 3, 3, 4]}>
+        <Tabs selected={props.defaultForm === "report" ? 1 : 0}>
+          {props.tabs.map(tab => (
+            <Pane label={tab.name} key={tab._id}>
+              {tab.content}
+            </Pane>
+          ))}
         </Tabs>
       </Box>
     </Block>
 
     <Block textCenter>
-      <Display color='brand' mb={1} children='Is it an emergency?' />
-      <LargeText color='text70' mb={3}>For all types of after-hours repairs.</LargeText>
+      <Display color="brand" mb={1} children="Is it an emergency?" />
+      <LargeText color="text70" mb={3}>
+        For all types of after-hours repairs.
+      </LargeText>
       <LargeButtonStyler>
-        <Link href='/emergency'>
-          <Button large icon bgColor='brand' children='Emergencies' />
+        <Link href="/emergency">
+          <Button large icon bgColor="brand" children="Emergencies" />
         </Link>
       </LargeButtonStyler>
     </Block>
 
     <Block border>
-      <ContactActionAlt btnColor='brandAlt' />
+      <ContactActionAlt btnColor="brandAlt" />
     </Block>
 
     <Footer />
   </Layout>
-)
+);
 
 class ContactPage extends React.Component {
   static async getInitialProps({ pathname, query }) {
     return {
       pathname,
-      defaultForm: query.form,
-    }
+      defaultForm: query.form
+    };
   }
 
   render() {
-    const tabs = [{
-      _id: 0,
-      name: 'Quick response',
-      content: <FormContact />
-    }, {
-      _id: 1,
-      name: 'Report an issue',
-      content: <FormIssue />
-    }];
+    const tabs = [
+      {
+        _id: 0,
+        name: "Quick response",
+        content: <FormContact />
+      },
+      {
+        _id: 1,
+        name: "Report an issue",
+        content: <FormIssue />
+      }
+    ];
 
-    return (
-      <Root pathname={this.props.pathname} tabs={tabs} {...this.props} />
-    )
+    return <Root pathname={this.props.pathname} tabs={tabs} {...this.props} />;
   }
 }
 
-export default ContactPage
+export default ContactPage;

782cba22c7e2d67051cb0940b0023f33d060d750
Barry Hood <BarryPH@protonmail.com>
2017-11-23 11:01:20 +1300

    Display warning message if browser user agent is IE11 or below

--- client/components/Shared/Layout.js
+++ client/components/Shared/Layout.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import theme from "../theme";
@@ -5,16 +6,35 @@ import globalStyles from "./global";
 
 // https://github.com/zeit/next.js/blob/master/examples/layout-component/components/layout.js
 
-export default ({ children, title = "VJ Ray Strata Management, Sydney" }) => (
-  <ThemeProvider theme={theme}>
-    <div>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+class Layout extends React.Component {
+  componentDidMount() {
+    // Display a warning message if cilent is using IE 11 or below
+    // React has compatibility issues with IE 11 or below
+    const ua = window.navigator.userAgent;
+    if (ua.includes('MSIE ') || ua.includes('Trident/')) {
+      alert(`This website doesn't work well on old Internet Explorer browsers. Please consider upgrading!`);
+    }
+  }
 
-      {children}
-    </div>
-  </ThemeProvider>
-);
+  render () {
+    return (
+      <ThemeProvider theme={theme}>
+        <div>
+          <Head>
+            <title>{this.props.title}</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
+
+          {this.props.children}
+        </div>
+      </ThemeProvider>
+    );
+  }
+};
+
+Layout.defaultProps = {
+  title: "VJ Ray Strata Management, Sydney",
+};
+
+export default Layout;
